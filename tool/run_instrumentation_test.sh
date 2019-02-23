f65af7db6d0c66ddb574000b7aad96f76abbf4f5
Maurice McCabe <mmcc007@gmail.com>
2019-02-22 20:20:50 -0800

    Added Flutter Gallery App

---
+++ tool/run_instrumentation_test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+if [ ! -f "./pubspec.yaml" ]; then
+  echo "ERROR: current directory must be the root of flutter_gallery package"
+  exit 1
+fi
+
+cd android
+
+# Currently there's no non-hacky way to pass a device ID to gradlew, but it's
+# OK as in the devicelab we have one device per host.
+#
+# See also: https://goo.gl/oe5aUW
+./gradlew connectedAndroidTest -Ptarget=test/live_smoketest.dart
+#./gradlew connectedAndroidTest -Ptarget=test_driver/transitions_perf_test.dart
+#./gradlew connectedAndroidTest -Ptarget=test/smoke_test.dart
